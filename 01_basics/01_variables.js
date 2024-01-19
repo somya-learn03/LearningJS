36866fad6c99783930983d871df482e1061c43de
somya-learn03 <157143510+somya-learn03@users.noreply.github.com>
2024-01-19 17:33:44 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accId = 1
+let accEmail = "blah"
+var accPassword = "123"   //never use var in javascript
+accCity = "Delhi"
+
+let accState;
+
+//accId = 2
+accEmail = "bullshit"
+accPassword = "345"
+accCity = "Noida"
+
+console.log(accId);
+console.table([accEmail,accPassword,accCity,accState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+//number
+//bigInt
+//string
+//boolean
+//null - standalone value
+//undefined
+//symbol - unique
+
+//object
+
+console.table([typeof undefined , typeof null])
