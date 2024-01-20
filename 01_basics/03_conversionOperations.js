e1166768a40b973da00043600eb45b2061cc6981
somya-learn03 <157143510+somya-learn03@users.noreply.github.com>
2024-01-20 16:37:48 +0000

    ops

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -4,3 +4,9 @@ console.log(typeof score)
 let newScore = Number(score)
 console.log(typeof newScore)
 console.log(newScore)     //NaN
+
+console.log("---------------------------------")
+
+console.log("2" === 2) //strict check compares datatypes as well
+console.log("2" == 2)
+
